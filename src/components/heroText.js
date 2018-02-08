921e2c5f7e13cd81597d5b0e4d8ef2b744bfef85
Darren Britton <darrenbritton@hotmail.com>
2018-02-08 00:43:41 +0000

    fixed styling hack for typed text

--- src/components/heroText.js
+++ src/components/heroText.js
@@ -4,33 +4,27 @@ import Typist from 'react-typist';
 
 import { media } from '../utils/style'
 
-const Base = styled.div`
-  color: #fff;
-  letter-spacing: 17px;
-  max-width: 100%;
-  font-size: 72px;
-  line-height: 80px;
-  font-weight: 100;
-  text-transform: uppercase;
-  font-family: 'Raleway';
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  ${ media.md`
-    left: 25%;
-    top: 40%;
-  ` }
-  ${ media.xs`
-    left: 3%;
-    top: 15%;
-    letter-spacing: 12px;
-  ` }
-`
-
 const StyledTypist = styled(Typist)`
-  .Cursor {
-    color: transparent;
-  }
+color: #fff;
+letter-spacing: 17px;
+max-width: 100%;
+font-size: 72px;
+line-height: 80px;
+font-weight: 100;
+text-transform: uppercase;
+font-family: 'Raleway';
+position: absolute;
+top: 50%;
+left: 50%;
+${ media.md`
+  left: 25%;
+  top: 40%;
+` }
+${ media.xs`
+  left: 3%;
+  top: 15%;
+  letter-spacing: 12px;
+` }
 `
 
 class HeroText extends React.Component {
@@ -38,31 +32,29 @@ class HeroText extends React.Component {
     if(this.props.text) {
       return (
         <Typist>
-          <Base {...this.props}>
+          <StyledTypist {...this.props}>
             <strong>{this.props.text.split(' ').slice(0,1)}<br /></strong>{this.props.text.split(' ').slice(1).join(' ')}
-          </Base>
+          </StyledTypist>
        </Typist>
       )
     } else {
       return (
-        <StyledTypist>
-          <Base {...this.props}>
-            <strong>I Make</strong> Websites
-            <Typist.Backspace count={9} delay={300} />
-            <span> Web Apps</span>
-            <Typist.Backspace count={9} delay={300} />
-            <span> Video Games</span>
-            <Typist.Backspace count={12} delay={300} />
-            <span> Poor Life Decisions</span>
-            <Typist.Delay ms={100} />
-            <span>.</span>
-            <Typist.Delay ms={200} />
-            <span>.</span>
-            <Typist.Delay ms={300} />
-            <span>.</span>
-            <Typist.Backspace count={29} delay={400} />
-            <strong>But Mostly Just</strong> Cool Web Stuff
-          </Base>
+        <StyledTypist cursor={{show: false}} {...this.props}>
+          <strong>I Make</strong> Websites
+          <Typist.Backspace count={9} delay={300} />
+          <span> Web Apps</span>
+          <Typist.Backspace count={9} delay={300} />
+          <span> Video Games</span>
+          <Typist.Backspace count={12} delay={300} />
+          <span> Poor Life Decisions</span>
+          <Typist.Delay ms={100} />
+          <span>.</span>
+          <Typist.Delay ms={200} />
+          <span>.</span>
+          <Typist.Delay ms={300} />
+          <span>.</span>
+          <Typist.Backspace count={29} delay={10} />
+          <strong>But Mostly Just</strong> Cool Web Stuff
        </StyledTypist>
       )
     }
