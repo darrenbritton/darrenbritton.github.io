47cc6adafb49e28e93994b821494c07677a7f7fa
Darren Britton <darrenbritton07@gmail.com>
2021-11-04 02:58:00 +0000

    visual: rhide icon mask

--- src/components/socialIcons.js
+++ src/components/socialIcons.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
-import FontAwesome from "react-fontawesome";import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from 'react-social-icons';
 
 const Base = styled.div`
   margin: 20px 0;
@@ -8,15 +8,17 @@ const Base = styled.div`
 `;
 
 const StyledSocialIcon = styled(SocialIcon)`
-  width: 20px !important;
-  height: 20px !important;
-  margin: 5px;
+  width: 35px !important;
+  height: 35px !important;
+  & .social-svg-mask {
+    display: none;
+  }
 `;
 
 class SocialIcons extends React.Component {
   render() {
     const icons = this.props.urls.map(function (url) {
-      return <StyledSocialIcon bgColor='#fff' url={url} />
+      return <StyledSocialIcon fgColor='#fff' url={url} />
       ;
     });
     return <Base {...this.props}>{icons}</Base>;
